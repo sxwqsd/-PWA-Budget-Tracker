06d715435e1479f5650ab69b42b1a107b8fcdb92
squid <mercado.valeria856@gmail.com>
2021-10-24 18:49:43 -0700

    i have done the manifest.json and the service- worker.js. have not tested it yet but we hope for the best

---
+++ Develop/public/service-worker.js
@@ -0,0 +1,58 @@
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+const FILES_TO_CACHE = [
+  "./index.html",
+  "./css/styles.css",
+  "./js/index.js",
+  "./js/idb.js",
+  "./manifest.json",
+  "./icons/icon-512x512.png",
+  "./icons/icon-384x384.png",
+  "./icons/icon-192x192.png",
+  "./icons/icon-152x152.png",
+  "./icons/icon-144x144.png",
+  "./icons/icon-128x128.png",
+  "./icons/icon-96x96.png",
+  "./icons/icon-72x72.png",
+];
+
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("beep boop! chaching files " + CACHE_NAME);
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("activate", function (evt) {
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("beep boop! deltetus the cache!", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function (e) {
+    console.log('retriving cache: ' + e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) { // if cache is available, respond with cache
+                console.log('respomging with da cache moneys baby: ' + e.request.url);
+                return request
+            } else {       // if there are no cache, try fetching request
+                console.log('it aint cache we fetching it again : ' + e.request.url);
+                return fetch(e.request)
+            }
+
+        })
+    )
+});
